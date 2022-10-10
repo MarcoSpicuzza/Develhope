c34f71dfecbb8c8265649c13d008f7240f80f3e1
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-10-10 15:56:16 +0200

    exercise 4 done

---
+++ node/4_modulesECMA/script.mjs
@@ -0,0 +1,9 @@
+const hello = (user) => {
+    console.log(`Hello ${user}`);
+}
+
+
+export default hello;
+
+// NODE
+// module.exports = hello;
---
+++ node/4_modulesECMA/useScript.mjs
@@ -0,0 +1,7 @@
+import hello from "./script.mjs";
+
+hello("Marco")
+
+// NODE
+// const helloUser = require("./script.js");
+// const helloUser = require("./script.js");
